d5f522b09c3a920407994981e04b0c30ea167a17
E-Persson-OSU <osuepersson@gmail.com>
2023-08-16 13:26:37 -0400

    working but unifinished

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 """main application file, possibly the only application file. python 3.11.4"""
 
-import urllib3
+from urllib3 import PoolManager
 import xmltodict
+
+
 
 FEED = "https://rpilocator.com/feed/"
 
 def getfeed():
-    """gets xml feed and converts it into a dict"""
-    http = urllib3.PoolManager()
+    """gets web feed and converts it into a list"""
+    # proxy = FreeProxy(country_id=['US'], rand=True, anonym=True).get()
+    # http = ProxyManager(proxy_url=proxy, headers=make_urllib_header())
+    http = PoolManager()
     response = http.request('GET', FEED)
-    print(response.read().decode('utf-8'))
-    xmldict = xmltodict.parse(response.read())
-    return xmldict
+    return response.data.decode('utf-8')
+
+def getitems(response):
+    """convert response to list of dicts containing information from the items"""
+    xml = xmltodict.parse(response)
+    listofdicts = xml['rss']['channel']['item']
+    return listofdicts
 
 def main():
     """main logic"""
-    xml = getfeed()
-    for i,k in xml:
-        print(i + ': ' + k)
+    resp = getfeed()
+    getitems(response=resp)
+    
 
 if __name__ == "__main__":
     main()
-
